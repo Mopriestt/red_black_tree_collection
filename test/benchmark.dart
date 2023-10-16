b5b94f58170cf1e1e913f0f43e259dd680967b3e
Yijun Li <Mopriestt@gmail.com>
2023-10-17 05:33:42 +1100

    Memory benchmarking

--- test/benchmark.dart
+++ test/benchmark.dart
@@ -18,50 +18,79 @@ List<String> get _dataSet => List.generate(_N, (_) => _randomString);
 void main() {
   final data = _dataSet;
 
-  test('SplayTreeSet 1 million add + 1 million query', () {
-    var start = DateTime.now().millisecondsSinceEpoch;
-    final splayTreeSet = SplayTreeSet<String>();
-    for (final s in data) {
-      splayTreeSet.add(s);
-    }
+  group('speed test', () {
+    test('SplayTreeSet 1 million add + 1 million query', () {
+      var start = DateTime.now().millisecondsSinceEpoch;
+      final splayTreeSet = SplayTreeSet<String>();
+      for (final s in data) {
+        splayTreeSet.add(s);
+      }
 
-    for (final s in data) {
-      splayTreeSet.contains(s);
-    }
-    var end = DateTime.now().millisecondsSinceEpoch;
-    print('\nSplayTreeSet 1 million add + 1 million search:');
-    print('${end - start} ms');
+      for (final s in data) {
+        splayTreeSet.contains(s);
+      }
+      var end = DateTime.now().millisecondsSinceEpoch;
+      print('\nSplayTreeSet 1 million add + 1 million search:');
+      print('${end - start} ms');
+    });
+
+    test('RBTreeSet 1 million add + 1 million query', () {
+      var start = DateTime.now().millisecondsSinceEpoch;
+      final rbTreeSet = RBTreeSet<String>();
+      for (final s in data) {
+        rbTreeSet.add(s);
+      }
+
+      for (final s in data) {
+        rbTreeSet.contains(s);
+      }
+      var end = DateTime.now().millisecondsSinceEpoch;
+      print('\nRbTreeSet 1 million add + 1 million search:');
+      print('${end - start} ms');
+    });
+
+    test('HashSet 1 million add + 1 million query', () {
+      var start = DateTime.now().millisecondsSinceEpoch;
+      final hashSet = HashSet<String>();
+      for (final s in data) {
+        hashSet.add(s);
+      }
+
+      for (final s in data) {
+        hashSet.contains(s);
+      }
+      var end = DateTime.now().millisecondsSinceEpoch;
+      print('\nHashSet 1 million add + 1 million search:');
+      print('${end - start} ms');
+    });
   });
 
-  test('RBTreeSet 1 million add + 1 million query', () {
-    final data = _dataSet;
-    var start = DateTime.now().millisecondsSinceEpoch;
-    final rbTreeSet = RBTreeSet<String>();
-    for (final s in data) {
-      rbTreeSet.add(s);
-    }
+  group('memory test', () {
+    test('SplayTreeSet memory test', () {
+      final sets = [
+        for (int i = 0; i < 1000; i++)
+          SplayTreeSet<String>.from(data.sublist(i * 1000, i * 1000 + 1000))
+      ];
 
-    for (final s in data) {
-      rbTreeSet.contains(s);
-    }
-    var end = DateTime.now().millisecondsSinceEpoch;
-    print('\nRbTreeSet 1 million add + 1 million search:');
-    print('${end - start} ms');
-  });
+      print(sets.length);
+    });
 
-  test('HashSet 1 million add + 1 million query', () {
-    final data = _dataSet;
-    var start = DateTime.now().millisecondsSinceEpoch;
-    final hashSet = HashSet<String>();
-    for (final s in data) {
-      hashSet.add(s);
-    }
+    test('RBTreeSet memory test', () {
+      final sets = [
+        for (int i = 0; i < 1000; i++)
+          RBTreeSet<String>.from(data.sublist(i * 1000, i * 1000 + 1000))
+      ];
 
-    for (final s in data) {
-      hashSet.contains(s);
-    }
-    var end = DateTime.now().millisecondsSinceEpoch;
-    print('\nHashSet 1 million add + 1 million search:');
-    print('${end - start} ms');
+      print(sets.length);
+    });
+
+    test('HashSet memory test', () {
+      final sets = [
+        for (int i = 0; i < 1000; i++)
+          HashSet<String>.from(data.sublist(i * 1000, i * 1000 + 1000))
+      ];
+
+      print(sets.length);
+    });
   });
 }
