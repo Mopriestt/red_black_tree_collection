f77b776dc0311c3045d76a3b1ceec8442904e01b
Yijun Li <Mopriestt@gmail.com>
2023-10-21 00:31:19 +1100

    Update license

--- lib/red_black_tree.dart
+++ lib/red_black_tree.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2023, Mopriestt.
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 library red_black_tree_collection;
 
 export 'src/red_black_tree.dart';
--- lib/src/rb_tree_set.dart
+++ lib/src/rb_tree_set.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2023, Mopriestt.
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 part of 'red_black_tree.dart';
 
 /// A node in a red black tree based set.
